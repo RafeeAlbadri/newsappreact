279f35f33ddf78c707eb4d445a3171382f22e990
dienasalah <45163700+dienasalah@users.noreply.github.com>
2018-11-19 12:54:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,67 @@
-import React,{Component} from 'React';
+import React, { Component } from 'React';
 import ReactDOM from 'react-dom';
 
 
- class Articles extends Component{
-    constructor(){
+class Articles extends Component {
+    constructor() {
 
         super()
 
         this.state = {
-            
-
+            art: []
         }
 
 
-     }
-        
+        this.getNews()
 
+    }
 
-   
-    
-        render(){
-            return (
+    async getNews() {
+        let response = await fetch(`https://newsapi.org/v2/everything?q=iraq&apiKey=978d6c3818ff431b8c210ae86550fb1f`)
+        let x = await response.json()
+        let art = this.state.art
+        this.setState({
+            art: x
+        })
+    }
 
-                <div className="header">
+    render() {
+        return (
+
+            <div className="container">
                 <img src="assets/logo.png" alt=""></img>
-                  <img id="mag" src="assets/mag.png" alt=""></img>
-                  <input placeholder="search a topic" type="text" name="" id="search"></input>
-               
-              
+                <img id="mag" src="assets/mag.png" alt=""></img>
+                <input placeholder="search a topic" type="text" name="" id="search"></input>
+
+
 
                 {
-                    this.state.art.map((item,i)=>{
+                    this.state.art.map((item, i) => {
 
-                        return 
-                        <div className="container">
-                        <img width="150px" height="150px"  src="{item.urlToImage}" alt=""></img>
-                        <h1>{item.title}</h1>
-                        <p>{item.description}</p>
-                        <time>{item.publishedAt}</time>
-                       </div>
+                        return
+                        <div className="ar" key="{i}">
+                            <img width="150px" height="150px" src="{item.urlToImage}" alt=""></img>
+                            <h1>{item.title}</h1>
+                            <p>{item.description}</p>
+                            <time>{item.publishedAt}</time>
+                        </div>
                     })
-                 }
-                </div>
-                
+                }
+            </div>
 
-            )
-                
-              
-            
-          }
-     }
+
+        )
+    }
+
+
+}
+
+// Define a statless components
+function App() {
+    return (
+        <Articles />
+    )
+}
+
+// Display content
+ReactDOM.render(<App />, document.getElementById('root'))
